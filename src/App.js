77f6b98d694efe2c1af4d04b5fdcc4005f6683a5
naveendonepudi21 <105103288+naveendonepudi21@users.noreply.github.com>
2022-05-07 03:19:15 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   const [weatherData, setWeatherData] = useState("");
   const [cityName, setCityName] = useState("");
-  const [next6days, setNext6days] = useState({ temps: [], days: [] });
+  const [next7days, setNext7days] = useState({ temps: [], days: [] });
 
   const handleWeatherData = (data) => {
     const daysData = data.list.map((day) => {
@@ -44,7 +44,7 @@ function App() {
 
     setWeatherData(data);
     setCityName(data.city.name);
-    setNext6days(forecast);
+    setNext7days(forecast);
   };
 
   const getDataWithName = async (cityName) => {
@@ -118,7 +118,7 @@ function App() {
                   </div>
                   <div className="w-full sm:max-w-1/2">
                     <p className="mb-4 text-center font-bold">Temperature - Next 7 days (Celsius)</p>
-                    <Chart next6days={next6days} />
+                    <Chart next7days={next7days} />
                   </div>
                 </div>
                 <div className="w-full flex flex-wrap justify-evenly">
